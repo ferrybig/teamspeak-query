469f9548a4a5d8a893cbc572e43d4e4c66ce9e4f
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2017-07-12 20:23:51 +0200

    Unit tests

---
+++ src/test/java/me/ferrybig/javacoding/teamspeakconnector/GroupTypeDataTest.java
@@ -0,0 +1,54 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Fernando van Loenhout.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package me.ferrybig.javacoding.teamspeakconnector;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import static org.junit.Assert.assertSame;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ *
+ * @author Fernando van Loenhout
+ */
+@RunWith(Parameterized.class)
+public class GroupTypeDataTest {
+
+	@Parameterized.Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.stream(Group.Type.values()).map(o -> new Object[]{o}).collect(Collectors.toList());
+	}
+
+	@Parameterized.Parameter
+	public Group.Type type;
+
+	@Test
+	public void getByIdWorksTest() {
+		assertSame(type, Group.Type.getById(type.getId()));
+	}
+
+}
