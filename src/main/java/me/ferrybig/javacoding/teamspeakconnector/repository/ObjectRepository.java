66043f61b6f900f77890f78708e6f4ade1309412
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2017-07-27 22:17:26 +0200

    Make repository system

---
+++ src/main/java/me/ferrybig/javacoding/teamspeakconnector/repository/ObjectRepository.java
@@ -0,0 +1,56 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Fernando van Loenhout.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package me.ferrybig.javacoding.teamspeakconnector.repository;
+
+import io.netty.util.concurrent.Future;
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ *
+ * @param <T> class that this repository contains
+ * @param <I> Id representing the types
+ */
+@ThreadSafe
+@ParametersAreNonnullByDefault
+public interface ObjectRepository<T, I> extends BaseRepository<T> {
+
+	@Nonnull
+	public default Future<?> deleteById(I id) {
+		return deleteById(id, false);
+	}
+
+	@Nonnull
+	public Future<?> deleteById(I id, boolean force);
+
+	@Nonnull
+	public default Future<T> getById(I id) {
+		return getById(id, false);
+	}
+
+	@Nonnull
+	public Future<T> getById(I id, boolean force);
+
+}
